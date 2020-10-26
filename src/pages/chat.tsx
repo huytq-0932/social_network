c57e7eeff3d46d146b2dc216c982f84aaa3ad0cb
luanphungba <luanphungba@gmail.com>
2020-10-26 10:59:14 +0700

    update

---
+++ src/pages/chat.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+class Chat extends Document {
+  render() {
+    return <div>Chat day nha</div>;
+  }
+}
+
+export default Chat;
