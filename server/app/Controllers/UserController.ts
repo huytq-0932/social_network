bfc94b3e8a7c7ff1c5c1eaa37f8b8510a9974b44
Tran Quang Huy <quanghuytran.hust@gmail.com>
2020-10-27 17:33:13 +0700

    Edit pass token via param of api

--- server/app/Controllers/UserController.ts
+++ server/app/Controllers/UserController.ts
@@ -5,106 +5,106 @@ import Auth from '@libs/Auth'
 import authConfig from '@config/auth'
 
 export default class Controller extends BaseController {
-    Model = Model
-    async signup(){
-        const inputs = this.request.all();
-        const allowFields = {
-            name: "string",
-            phonenumber: "string!",
-            password: "string!",
-            uuid: "string!",
-        }
-        const data = this.validate(inputs, allowFields);
-        let exist = await this.Model.query().findOne({phone: data.phonenumber});
-        if(exist){
-            throw new ApiException(9996, "User Exist");
-        }
-        data.password = this.Model.hash(data.password);
-        
-        let result = await this.Model.query().insert({
-            phone: data.phone,
-            password: data.password,
-            name: data.name || "new member"
-        });
-        delete result.password;
-        return result;
+  Model = Model
+
+  async signup() {
+    const inputs = this.request.all();
+    const allowFields = {
+      name: "string",
+      phonenumber: "string!",
+      password: "string!",
+      uuid: "string!",
     }
-    async login() {
-        const inputs = this.request.all();
-        const allowFields = {
-            phonenumber: "string!",
-            password: "string!"
-        }
+    const data = this.validate(inputs, allowFields);
+    let exist = await this.Model.query().findOne({phone: data.phonenumber});
+    if (exist) {
+      throw new ApiException(9996, "User Exist");
+    }
+    data.password = this.Model.hash(data.password);
 
-        const data = this.validate(inputs, allowFields);
-        let user = await this.Model.checkLogin({
-            phonenumber: data.phonenumber,
-            password: data.password
-        })
-        if (!user) {
-            throw new ApiException(7000, "Can not login")
-        }
-        let token = Auth.generateJWT({
-            id: user.id,
-            phonenumber: user.phone
-        }, {
-            key: authConfig['SECRET_KEY'],
-            expiresIn: authConfig['JWT_EXPIRE']
-        });
+    let result = await this.Model.query().insert({
+      phone: data.phone,
+      password: data.password,
+      name: data.name || "new member"
+    });
+    delete result.password;
+    return result;
+  }
 
-        this.response.success({
-            id: user.id,
-            username: user.name,
-            token,
-            avatar: user.avatar,
-            active: (!user.avatar && !user.name) ? -1 : 1
-        })
+  async login() {
+    const inputs = this.request.all();
+    const allowFields = {
+      phonenumber: "string!",
+      password: "string!"
     }
 
-    async updatePassword() {
-        let inputs = this.request.all()
+    const data = this.validate(inputs, allowFields);
+    let user = await this.Model.checkLogin({
+      phonenumber: data.phonenumber,
+      password: data.password
+    })
+    if (!user) {
+      throw new ApiException(7000, "Can not login")
+    }
+    let token = Auth.generateJWT({
+      id: user.id,
+      phonenumber: user.phone
+    }, {
+      key: authConfig['SECRET_KEY_ADMIN'],
+      expiresIn: authConfig['JWT_EXPIRE_ADMIN']
+    });
 
-        const allowFields = {
-            password: "string!",
-            token: "string!"
-        }
-        let data = this.validate(inputs, allowFields, {
-            removeNotAllow: true
-        });
-        let verifyToken = await Auth.verify(data.token);
-        console.log("verify ", verifyToken);
-        return verifyToken;
-        // let user = await this.Model.query().findById(id);
+    this.response.success({
+      id: user.id,
+      username: user.name,
+      token,
+      avatar: user.avatar,
+      active: (!user.avatar && !user.name) ? -1 : 1
+    })
+  }
 
-        // if (!user) {
-        //     throw new ApiException(9995, "User is not validated")
-        // }
+  async updateMyPassword() {
+    let inputs = this.request.all()
 
-        // let result = await user.changePassword(data['password'])
-        // delete result['password']
-        // return result
+    const allowFields = {
+      password: "string!"
+    }
+    let data = this.validate(inputs, allowFields, {
+      removeNotAllow: true
+    });
+    const auth = this.request.auth || {};
+    const id = auth.id;
+    let user = await this.Model.query().findById(id);
+
+    if (!user) {
+      throw new ApiException(9995, "User is not validated")
     }
 
-    async getInfo() {
-        const inputs = this.request.all();
-        const allowFields = {
-            token: "string!",
-            user_id: "string"
-        }
-        const data = this.validate(inputs, allowFields);
-        try {
-            const decodedToken = await Auth.decodeJWT(data.token, {
-                key: authConfig['SECRET_KEY'],
-                expiresIn: authConfig['JWT_EXPIRE']
-            })
-            const id = !data.user_id ? decodedToken.id : data.user_id
-            const user = await this.Model.getInfo(id)
-            if (!user) {
-                throw new ApiException(9995, "User is not validated")
-            }
-            this.response.success(user)
-        } catch (e) {
-            throw new ApiException(9998, "Token is invalid")
-        }
+    let result = await user.changePassword(data['password'])
+    delete result['password']
+    return result
+  }
+
+  async getInfo() {
+    const inputs = this.request.all();
+    const allowFields = {
+      token: "string!",
+      user_id: "string"
     }
+    const data = this.validate(inputs, allowFields);
+    try {
+      const decodedToken = await Auth.decodeJWT(data.token, {
+        key: authConfig['SECRET_KEY_ADMIN'],
+        expiresIn: authConfig['JWT_EXPIRE_ADMIN']
+      })
+      const id = !data.user_id ? decodedToken.id : data.user_id
+      const user = await this.Model.getInfo(id)
+      if (!user) {
+        throw new ApiException(9995, "User is not validated")
+      }
+      this.response.success(user)
+    } catch (e) {
+      throw new ApiException(9998, "Token is invalid")
+    }
+  }
 }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,16 +2,17 @@ import Route from '@core/Routes'
 
 const ExtendMiddleware = require("@app/Middlewares/ExtendMiddleware");
 const AuthApiMiddleware = require('@app/Middlewares/AuthApiMiddleware')
-const { permission, permissionResource, permissionMethod } = require('@app/Middlewares/PermissionMiddleware')
-  
+const {permission, permissionResource, permissionMethod} = require('@app/Middlewares/PermissionMiddleware')
+
 Route.group(() => {
   Route.post("/login", "UserController.login").name('login')
   Route.post("/signup", "UserController.signup").name('signup')
-  
+
+  Route.get("/get_user_info", "UserController.getInfo").name('getInfo')
+
   Route.group(() => {
     Route.put("/change_password", "UserController.updatePassword").name('updatePassword')
     Route.resource("/users", "UserController").name('users')
-    Route.resource("/get_user_info", "UserController.getInfo").name('getInfo')
   }).middleware([AuthApiMiddleware])
 
   
